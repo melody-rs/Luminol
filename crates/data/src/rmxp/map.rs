84e7ef7356159b08ac1bbb3c67ccbc65e9ad6be8
melody-rs <melody@melody-is.gay>
2025-02-10 06:34:50 -0800

    it works, but at what cost?

--- crates/data/src/rmxp/map.rs
+++ crates/data/src/rmxp/map.rs
@@ -1,6 +1,4 @@
-use alox_48::{DeError, SerializeIvars};
-use serde::de::Error;
-use serde::ser::SerializeStruct;
+use alox_48::{DeError, SerializeHash, SerializeIvars};
 
 // Copyright (C) 2024 Melody Madeline Lyons
 //
@@ -19,11 +17,10 @@ use serde::ser::SerializeStruct;
 // You should have received a copy of the GNU General Public License
 // along with Luminol.  If not, see <http://www.gnu.org/licenses/>.
 use crate::rpg::{AudioFile, Event};
-use crate::{id_alox, id_serde, option_vec, Table3};
+use crate::{id_alox, option_vec, Table3};
 
-#[derive(Default, Debug, serde::Deserialize, serde::Serialize)]
+#[derive(Default, Debug)]
 pub struct Map {
-    #[serde(with = "id_serde")]
     pub tileset_id: usize,
     pub width: usize,
     pub height: usize,
@@ -36,7 +33,6 @@ pub struct Map {
     pub data: Table3,
     pub events: option_vec::OptionVec<Event>,
 
-    #[serde(skip)]
     pub modified: bool,
 }
 
@@ -46,6 +42,51 @@ pub struct MapDeserializer<'res> {
 
 struct Visitor<'res> {
     command_db: &'res crate::CommandDB,
+}
+
+struct MultiEventDeserializer<'res> {
+    command_db: &'res crate::CommandDB,
+}
+
+impl<'res, 'de> alox_48::de::DeserializeSeed<'de> for MultiEventDeserializer<'res> {
+    type Value = option_vec::OptionVec<Event>;
+
+    fn deserialize<D>(self, deserializer: D) -> alox_48::DeResult<Self::Value>
+    where
+        D: alox_48::DeserializerTrait<'de>,
+    {
+        struct Visitor<'res> {
+            command_db: &'res crate::CommandDB,
+        }
+        impl<'res, 'de> alox_48::de::Visitor<'de> for Visitor<'res> {
+            type Value = option_vec::OptionVec<Event>;
+
+            fn expecting(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                formatter.write_str("an array of RPG::Event")
+            }
+
+            fn visit_hash<A>(self, mut map: A) -> alox_48::DeResult<Self::Value>
+            where
+                A: alox_48::HashAccess<'de>,
+            {
+                let Self { command_db } = self;
+                // cleanest way I could find of doing it
+                std::iter::from_fn(|| {
+                    let index = match map.next_key().transpose()? {
+                        Ok(index) => index,
+                        Err(e) => return Some(Err(e)),
+                    };
+                    let seed = crate::rpg::EventDeserializer { command_db };
+                    let result = map.next_value_seed(seed).map(|event| (index, event));
+                    Some(result)
+                })
+                .collect()
+            }
+        }
+
+        let Self { command_db } = self;
+        deserializer.deserialize(Visitor { command_db })
+    }
 }
 
 impl<'res, 'de> alox_48::de::Visitor<'de> for Visitor<'res> {
@@ -63,6 +104,7 @@ impl<'res, 'de> alox_48::de::Visitor<'de> for Visitor<'res> {
     where
         A: alox_48::IvarAccess<'de>,
     {
+        let Self { command_db } = self;
         let mut tileset_id = None;
         let mut width = None;
         let mut height = None;
@@ -91,7 +133,9 @@ impl<'res, 'de> alox_48::de::Visitor<'de> for Visitor<'res> {
                 "encounter_step" => encounter_step = Some(instance_variables.next_value()?),
                 "data" => data = Some(instance_variables.next_value()?),
                 "events" => {
-                    todo!();
+                    let seed = MultiEventDeserializer { command_db };
+                    let value = instance_variables.next_value_seed(seed)?;
+                    events = Some(value);
                 }
                 _ => {
                     instance_variables.next_value::<alox_48::de::Ignored>()?;
@@ -120,131 +164,6 @@ impl<'res, 'de> alox_48::de::Visitor<'de> for Visitor<'res> {
     }
 }
 
-enum Field {
-    TilesetId,
-    Width,
-    Height,
-    AutoplayBgm,
-    Bgm,
-    AutoplayBgs,
-    Bgs,
-    EncounterList,
-    EncounterStep,
-    Data,
-    Events,
-    Ignore,
-}
-
-struct FieldVisitor;
-
-impl<'de> serde::de::Visitor<'de> for FieldVisitor {
-    type Value = Field;
-
-    fn expecting(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        formatter.write_str("field identifier")
-    }
-
-    fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
-    where
-        E: serde::de::Error,
-    {
-        self.visit_bytes(v.as_bytes())
-    }
-
-    fn visit_bytes<E>(self, v: &[u8]) -> Result<Self::Value, E>
-    where
-        E: serde::de::Error,
-    {
-        match v {
-            b"tileset_id" => Ok(Field::TilesetId),
-            b"width" => Ok(Field::Width),
-            b"height" => Ok(Field::Height),
-            b"autoplay_bgm" => Ok(Field::AutoplayBgm),
-            b"bgm" => Ok(Field::Bgm),
-            b"autoplay_bgs" => Ok(Field::AutoplayBgs),
-            b"bgs" => Ok(Field::Bgs),
-            b"encounter_list" => Ok(Field::EncounterList),
-            b"encounter_step" => Ok(Field::EncounterStep),
-            b"data" => Ok(Field::Data),
-            b"events" => Ok(Field::Events),
-            _ => Ok(Field::Ignore),
-        }
-    }
-}
-
-impl<'de> serde::Deserialize<'de> for Field {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: serde::Deserializer<'de>,
-    {
-        deserializer.deserialize_identifier(FieldVisitor)
-    }
-}
-
-impl<'res, 'de> serde::de::Visitor<'de> for Visitor<'res> {
-    type Value = Map;
-
-    fn expecting(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        formatter.write_str("struct Map")
-    }
-
-    fn visit_map<A>(self, mut map: A) -> Result<Self::Value, A::Error>
-    where
-        A: serde::de::MapAccess<'de>,
-    {
-        let mut tileset_id = None;
-        let mut width = None;
-        let mut height = None;
-        let mut autoplay_bgm = None;
-        let mut bgm = None;
-        let mut autoplay_bgs = None;
-        let mut bgs = None;
-        let mut encounter_list = None;
-        let mut encounter_step = None;
-        let mut data = None;
-        let mut events = None;
-
-        while let Some(field) = map.next_key()? {
-            match field {
-                Field::TilesetId => {
-                    let id = map.next_value::<id_serde::DeserializeId>()?;
-                    tileset_id = Some(id.0);
-                }
-                Field::Width => width = Some(map.next_value()?),
-                Field::Height => height = Some(map.next_value()?),
-                Field::AutoplayBgm => autoplay_bgm = Some(map.next_value()?),
-                Field::Bgm => bgm = Some(map.next_value()?),
-                Field::AutoplayBgs => autoplay_bgs = Some(map.next_value()?),
-                Field::Bgs => bgs = Some(map.next_value()?),
-                Field::EncounterList => encounter_list = Some(map.next_value()?),
-                Field::EncounterStep => encounter_step = Some(map.next_value()?),
-                Field::Data => data = Some(map.next_value()?),
-                Field::Events => {
-                    todo!();
-                }
-                Field::Ignore => {}
-            }
-        }
-
-        Ok(Map {
-            tileset_id: tileset_id.ok_or_else(|| A::Error::missing_field("tileset_id"))?,
-            width: width.ok_or_else(|| A::Error::missing_field("width"))?,
-            height: height.ok_or_else(|| A::Error::missing_field("height"))?,
-            autoplay_bgm: autoplay_bgm.ok_or_else(|| A::Error::missing_field("autoplay_bgm"))?,
-            bgm: bgm.ok_or_else(|| A::Error::missing_field("bgm"))?,
-            autoplay_bgs: autoplay_bgs.ok_or_else(|| A::Error::missing_field("autoplay_bgs"))?,
-            bgs: bgs.ok_or_else(|| A::Error::missing_field("bgs"))?,
-            encounter_list: encounter_list
-                .ok_or_else(|| A::Error::missing_field("encounter_list"))?,
-            encounter_step: encounter_step
-                .ok_or_else(|| A::Error::missing_field("encounter_step"))?,
-            data: data.ok_or_else(|| A::Error::missing_field("data"))?,
-            events: events.ok_or_else(|| A::Error::missing_field("events"))?,
-            modified: false,
-        })
-    }
-}
-
 impl<'res, 'de> alox_48::de::DeserializeSeed<'de> for MapDeserializer<'res> {
     type Value = Map;
 
@@ -252,7 +171,7 @@ impl<'res, 'de> alox_48::de::DeserializeSeed<'de> for MapDeserializer<'res> {
     where
         D: alox_48::DeserializerTrait<'de>,
     {
-        let MapDeserializer { command_db } = self;
+        let Self { command_db } = self;
         deserializer.deserialize(Visitor { command_db })
     }
 }
@@ -264,21 +183,7 @@ impl<'res, 'de> serde::de::DeserializeSeed<'de> for MapDeserializer<'res> {
     where
         D: serde::Deserializer<'de>,
     {
-        let fields = &[
-            "tileset_id",
-            "width",
-            "height",
-            "autoplay_bgm",
-            "bgm",
-            "autoplay_bgs",
-            "bgs",
-            "encounter_list",
-            "encounter_step",
-            "data",
-            "events",
-        ];
-        let MapDeserializer { command_db } = self;
-        deserializer.deserialize_struct("Map", fields, Visitor { command_db })
+        todo!()
     }
 }
 
@@ -292,22 +197,59 @@ impl<'res> alox_48::ser::Serialize for MapSerializer<'res> {
     where
         S: alox_48::SerializerTrait,
     {
-        let MapSerializer { command_db, map } = *self;
+        let Self { command_db, map } = *self;
+        let Map {
+            tileset_id,
+            width,
+            height,
+            autoplay_bgm,
+            bgm,
+            autoplay_bgs,
+            bgs,
+            encounter_list,
+            encounter_step,
+            data,
+            events,
+            ..
+        } = map;
         let mut ivars = serializer.serialize_object("RPG::Map".into(), 11)?;
 
-        ivars.serialize_entry("@tileset_id".into(), &id_alox::SerializeId(map.tileset_id))?;
-        ivars.serialize_entry("@width".into(), &map.width)?;
-        ivars.serialize_entry("@height".into(), &map.height)?;
-        ivars.serialize_entry("@autoplay_bgm".into(), &map.autoplay_bgm)?;
-        ivars.serialize_entry("@bgm".into(), &map.bgm)?;
-        ivars.serialize_entry("@autoplay_bgs".into(), &map.autoplay_bgs)?;
-        ivars.serialize_entry("@bgs".into(), &map.bgs)?;
-        ivars.serialize_entry("@encounter_list".into(), &map.encounter_list)?;
-        ivars.serialize_entry("@encounter_step".into(), &map.encounter_step)?;
-        ivars.serialize_entry("@data".into(), &map.data)?;
-        ivars.serialize_entry("@events".into(), &map.events)?;
+        ivars.serialize_entry("@tileset_id".into(), &id_alox::SerializeId(*tileset_id))?;
+        ivars.serialize_entry("@width".into(), width)?;
+        ivars.serialize_entry("@height".into(), height)?;
+        ivars.serialize_entry("@autoplay_bgm".into(), autoplay_bgm)?;
+        ivars.serialize_entry("@bgm".into(), bgm)?;
+        ivars.serialize_entry("@autoplay_bgs".into(), autoplay_bgs)?;
+        ivars.serialize_entry("@bgs".into(), bgs)?;
+        ivars.serialize_entry("@encounter_list".into(), encounter_list)?;
+        ivars.serialize_entry("@encounter_step".into(), encounter_step)?;
+        ivars.serialize_entry("@data".into(), data)?;
+        ivars.serialize_entry(
+            "@events".into(),
+            &MultiEventSerializer { command_db, events },
+        )?;
 
         ivars.end()
+    }
+}
+
+struct MultiEventSerializer<'res> {
+    command_db: &'res crate::CommandDB,
+    events: &'res option_vec::OptionVec<Event>,
+}
+
+impl<'res> alox_48::Serialize for MultiEventSerializer<'res> {
+    fn serialize<S>(&self, serializer: S) -> alox_48::SerResult<S::Ok>
+    where
+        S: alox_48::SerializerTrait,
+    {
+        let Self { command_db, events } = *self;
+        let mut ser = serializer.serialize_hash(events.size())?;
+        for (index, event) in events {
+            ser.serialize_key(&index)?;
+            ser.serialize_value(&crate::rpg::EventSerializer { command_db, event })?;
+        }
+        ser.end()
     }
 }
 
@@ -316,21 +258,6 @@ impl<'res> serde::Serialize for MapSerializer<'res> {
     where
         S: serde::Serializer,
     {
-        let MapSerializer { command_db, map } = *self;
-        let mut fields = serializer.serialize_struct("Map", 11)?;
-
-        fields.serialize_field("tileset_id", &id_serde::SerializeId(map.tileset_id))?;
-        fields.serialize_field("width", &map.width)?;
-        fields.serialize_field("height", &map.height)?;
-        fields.serialize_field("autoplay_bgm", &map.autoplay_bgm)?;
-        fields.serialize_field("bgm", &map.bgm)?;
-        fields.serialize_field("autoplay_bgs", &map.autoplay_bgs)?;
-        fields.serialize_field("bgs", &map.bgs)?;
-        fields.serialize_field("encounter_list", &map.encounter_list)?;
-        fields.serialize_field("encounter_step", &map.encounter_step)?;
-        fields.serialize_field("data", &map.data)?;
-        fields.serialize_field("events", &map.events)?;
-
-        fields.end()
+        todo!()
     }
 }
--- crates/data/src/shared/event.rs
+++ crates/data/src/shared/event.rs
@@ -18,21 +18,16 @@ use crate::{
     id_alox, id_serde, optional_id_alox, optional_id_serde, optional_path_alox,
     optional_path_serde, rpg::MoveRoute, BlendMode, ParameterType, Path,
 };
+use alox_48::{DeError, SerializeIvars};
 
-#[derive(Debug, serde::Deserialize, serde::Serialize, Clone)]
-#[derive(alox_48::Deserialize, alox_48::Serialize)]
-#[marshal(class = "RPG::Event")]
+#[derive(Debug, Clone)]
 pub struct Event {
-    // #[serde(with = "id_serde")]
-    // #[marshal(with = "id_alox")]
     pub id: usize,
     pub name: String,
     pub x: i32,
     pub y: i32,
     pub pages: Vec<EventPage>,
 
-    #[serde(skip)]
-    #[marshal(skip)]
     pub extra_data: EventExtraData,
 }
 
@@ -71,9 +66,7 @@ pub struct CommonEvent {
     pub list: Vec<EventCommand>,
 }
 
-#[derive(Debug, serde::Deserialize, serde::Serialize, Clone)]
-#[derive(alox_48::Deserialize, alox_48::Serialize)]
-#[marshal(class = "RPG::Event::Page")]
+#[derive(Debug, Clone)]
 pub struct EventPage {
     pub condition: EventCondition,
     pub graphic: Graphic,
@@ -292,4 +285,303 @@ pub struct EventCommand {
     pub code: u16,
     pub indent: usize,
     pub parameters: Vec<ParameterType>,
+}
+
+pub struct EventDeserializer<'res> {
+    pub command_db: &'res crate::CommandDB,
+}
+
+struct EventVisitor<'res> {
+    command_db: &'res crate::CommandDB,
+}
+
+impl<'res, 'de> alox_48::de::DeserializeSeed<'de> for EventDeserializer<'res> {
+    type Value = Event;
+
+    fn deserialize<D>(self, deserializer: D) -> alox_48::DeResult<Self::Value>
+    where
+        D: alox_48::DeserializerTrait<'de>,
+    {
+        let Self { command_db } = self;
+        deserializer.deserialize(EventVisitor { command_db })
+    }
+}
+
+impl<'res, 'de> alox_48::de::Visitor<'de> for EventVisitor<'res> {
+    type Value = Event;
+
+    fn expecting(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        formatter.write_str("an instance of RPG::Event")
+    }
+
+    fn visit_object<A>(
+        self,
+        _: &'de alox_48::Sym,
+        mut instance_variables: A,
+    ) -> alox_48::DeResult<Self::Value>
+    where
+        A: alox_48::IvarAccess<'de>,
+    {
+        let Self { command_db } = self;
+        let mut id = None;
+        let mut name = None;
+        let mut x = None;
+        let mut y = None;
+        let mut pages = None;
+
+        while let Some(field) = instance_variables.next_ivar()? {
+            match field.to_rust_field_name().unwrap_or(field).as_str() {
+                "id" => id = Some(instance_variables.next_value()?),
+                "name" => name = Some(instance_variables.next_value()?),
+                "x" => x = Some(instance_variables.next_value()?),
+                "y" => y = Some(instance_variables.next_value()?),
+                "pages" => {
+                    let seed = MultiPageDeserializer { command_db };
+                    let value = instance_variables.next_value_seed(seed)?;
+                    pages = Some(value);
+                }
+                _ => {
+                    instance_variables.next_value::<alox_48::de::Ignored>()?;
+                }
+            }
+        }
+
+        Ok(Event {
+            id: id.ok_or_else(|| DeError::missing_field("id".into()))?,
+            name: name.ok_or_else(|| DeError::missing_field("name".into()))?,
+            x: x.ok_or_else(|| DeError::missing_field("x".into()))?,
+            y: y.ok_or_else(|| DeError::missing_field("y".into()))?,
+            pages: pages.ok_or_else(|| DeError::missing_field("pages".into()))?,
+            extra_data: EventExtraData::default(),
+        })
+    }
+}
+
+struct MultiPageDeserializer<'res> {
+    command_db: &'res crate::CommandDB,
+}
+
+impl<'res, 'de> alox_48::de::DeserializeSeed<'de> for MultiPageDeserializer<'res> {
+    type Value = Vec<EventPage>;
+
+    fn deserialize<D>(self, deserializer: D) -> alox_48::DeResult<Self::Value>
+    where
+        D: alox_48::DeserializerTrait<'de>,
+    {
+        struct Visitor<'res> {
+            command_db: &'res crate::CommandDB,
+        }
+        impl<'res, 'de> alox_48::de::Visitor<'de> for Visitor<'res> {
+            type Value = Vec<EventPage>;
+
+            fn expecting(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                formatter.write_str("an array of RPG::Event::Page")
+            }
+
+            fn visit_array<A>(self, mut array: A) -> alox_48::DeResult<Self::Value>
+            where
+                A: alox_48::ArrayAccess<'de>,
+            {
+                let Self { command_db } = self;
+                let mut data = Vec::with_capacity(array.len());
+                while let Some(page) =
+                    array.next_element_seed(EventPageDeserializer { command_db })?
+                {
+                    data.push(page);
+                }
+                Ok(data)
+            }
+        }
+
+        let Self { command_db } = self;
+        deserializer.deserialize(Visitor { command_db })
+    }
+}
+
+struct EventPageDeserializer<'res> {
+    command_db: &'res crate::CommandDB,
+}
+
+struct EventPageVisitor<'res> {
+    command_db: &'res crate::CommandDB,
+}
+
+impl<'res, 'de> alox_48::de::Visitor<'de> for EventPageVisitor<'res> {
+    type Value = EventPage;
+
+    fn expecting(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        formatter.write_str("an instance of RPG::Event::Page")
+    }
+
+    fn visit_object<A>(
+        self,
+        _: &'de alox_48::Sym,
+        mut instance_variables: A,
+    ) -> alox_48::DeResult<Self::Value>
+    where
+        A: alox_48::IvarAccess<'de>,
+    {
+        let Self { command_db } = self;
+        let mut condition = None;
+        let mut graphic = None;
+        let mut move_type = None;
+        let mut move_speed = None;
+        let mut move_frequency = None;
+        let mut move_route = None;
+        let mut walk_anime = None;
+        let mut step_anime = None;
+        let mut direction_fix = None;
+        let mut through = None;
+        let mut always_on_top = None;
+        let mut trigger = None;
+        let mut list = None;
+
+        while let Some(field) = instance_variables.next_ivar()? {
+            match field.to_rust_field_name().unwrap_or(field).as_str() {
+                "condition" => condition = Some(instance_variables.next_value()?),
+                "graphic" => graphic = Some(instance_variables.next_value()?),
+                "move_type" => move_type = Some(instance_variables.next_value()?),
+                "move_speed" => move_speed = Some(instance_variables.next_value()?),
+                "move_frequency" => move_frequency = Some(instance_variables.next_value()?),
+                "move_route" => move_route = Some(instance_variables.next_value()?),
+                "walk_anime" => walk_anime = Some(instance_variables.next_value()?),
+                "step_anime" => step_anime = Some(instance_variables.next_value()?),
+                "direction_fix" => direction_fix = Some(instance_variables.next_value()?),
+                "through" => through = Some(instance_variables.next_value()?),
+                "always_on_top" => always_on_top = Some(instance_variables.next_value()?),
+                "trigger" => trigger = Some(instance_variables.next_value()?),
+                "list" => {
+                    // TODO
+                    list = Some(instance_variables.next_value()?);
+                }
+                _ => {
+                    instance_variables.next_value::<alox_48::de::Ignored>()?;
+                }
+            }
+        }
+
+        Ok(EventPage {
+            condition: condition.ok_or_else(|| DeError::missing_field("condition".into()))?,
+            graphic: graphic.ok_or_else(|| DeError::missing_field("graphic".into()))?,
+            move_type: move_type.ok_or_else(|| DeError::missing_field("move_type".into()))?,
+            move_speed: move_speed.ok_or_else(|| DeError::missing_field("move_speed".into()))?,
+            move_frequency: move_frequency
+                .ok_or_else(|| DeError::missing_field("move_frequency".into()))?,
+            move_route: move_route.ok_or_else(|| DeError::missing_field("move_route".into()))?,
+            walk_anime: walk_anime.ok_or_else(|| DeError::missing_field("walk_anime".into()))?,
+            step_anime: step_anime.ok_or_else(|| DeError::missing_field("step_anime".into()))?,
+            direction_fix: direction_fix
+                .ok_or_else(|| DeError::missing_field("direction_fix".into()))?,
+            through: through.ok_or_else(|| DeError::missing_field("through".into()))?,
+            always_on_top: always_on_top
+                .ok_or_else(|| DeError::missing_field("always_on_top".into()))?,
+            trigger: trigger.ok_or_else(|| DeError::missing_field("trigger".into()))?,
+            list: list.ok_or_else(|| DeError::missing_field("list".into()))?,
+        })
+    }
+}
+
+impl<'res, 'de> alox_48::de::DeserializeSeed<'de> for EventPageDeserializer<'res> {
+    type Value = EventPage;
+
+    fn deserialize<D>(self, deserializer: D) -> alox_48::DeResult<Self::Value>
+    where
+        D: alox_48::DeserializerTrait<'de>,
+    {
+        let Self { command_db } = self;
+        deserializer.deserialize(EventPageVisitor { command_db })
+    }
+}
+
+pub struct EventSerializer<'res> {
+    pub command_db: &'res crate::CommandDB,
+    pub event: &'res Event,
+}
+
+impl<'res> alox_48::Serialize for EventSerializer<'res> {
+    fn serialize<S>(&self, serializer: S) -> alox_48::SerResult<S::Ok>
+    where
+        S: alox_48::SerializerTrait,
+    {
+        let Self { command_db, event } = *self;
+        let Event {
+            id,
+            name,
+            x,
+            y,
+            pages,
+            ..
+        } = event;
+        let mut ivars = serializer.serialize_object("RPG::Event".into(), 5)?;
+
+        ivars.serialize_entry("@id".into(), id)?;
+        ivars.serialize_entry("@name".into(), name)?;
+        ivars.serialize_entry("@x".into(), x)?;
+        ivars.serialize_entry("@y".into(), y)?;
+        ivars.serialize_entry("@pages".into(), &MultiPageSerializer { command_db, pages })?;
+
+        ivars.end()
+    }
+}
+
+struct MultiPageSerializer<'res> {
+    pub command_db: &'res crate::CommandDB,
+    pub pages: &'res [EventPage],
+}
+
+impl<'res> alox_48::Serialize for MultiPageSerializer<'res> {
+    fn serialize<S>(&self, serializer: S) -> alox_48::SerResult<S::Ok>
+    where
+        S: alox_48::SerializerTrait,
+    {
+        let Self { command_db, pages } = *self;
+        let iter = pages.iter().map(|page| PageSerializer { page, command_db });
+        serializer.collect_array(iter)
+    }
+}
+
+struct PageSerializer<'res> {
+    pub command_db: &'res crate::CommandDB,
+    pub page: &'res EventPage,
+}
+
+impl<'res> alox_48::Serialize for PageSerializer<'res> {
+    fn serialize<S>(&self, serializer: S) -> alox_48::SerResult<S::Ok>
+    where
+        S: alox_48::SerializerTrait,
+    {
+        let Self { command_db, page } = *self;
+        let EventPage {
+            condition,
+            graphic,
+            move_type,
+            move_speed,
+            move_frequency,
+            move_route,
+            walk_anime,
+            step_anime,
+            direction_fix,
+            through,
+            always_on_top,
+            trigger,
+            list,
+        } = page;
+        let mut ivars = serializer.serialize_object("RPG::Event::Page".into(), 13)?;
+
+        ivars.serialize_entry("@condition".into(), condition)?;
+        ivars.serialize_entry("@graphic".into(), graphic)?;
+        ivars.serialize_entry("@move_type".into(), move_type)?;
+        ivars.serialize_entry("@move_speed".into(), move_speed)?;
+        ivars.serialize_entry("@move_frequency".into(), move_frequency)?;
+        ivars.serialize_entry("@move_route".into(), move_route)?;
+        ivars.serialize_entry("@walk_anime".into(), walk_anime)?;
+        ivars.serialize_entry("@step_anime".into(), step_anime)?;
+        ivars.serialize_entry("@direction_fix".into(), direction_fix)?;
+        ivars.serialize_entry("@through".into(), through)?;
+        ivars.serialize_entry("@always_on_top".into(), always_on_top)?;
+        ivars.serialize_entry("@trigger".into(), trigger)?;
+        ivars.serialize_entry("@list".into(), list)?;
+
+        ivars.end()
+    }
 }
